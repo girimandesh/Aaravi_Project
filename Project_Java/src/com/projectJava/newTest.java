16e2222b6e4fa5977f02932d35510ea7db03281d
Owner <Owner@Mandesh.fios-router.home>
2020-09-10 10:35:41 -0400

    New Line Added

--- Project_Java/src/com/projectJava/newTest.java
+++ Project_Java/src/com/projectJava/newTest.java
@@ -3,6 +3,7 @@ package com.projectJava;
 public class newTest {
 	public static void main(String[] args) {
 		System.out.println("This is for Testing");
+		System.out.println("My Name is Mandesh Giri");
 	}
 
 }
