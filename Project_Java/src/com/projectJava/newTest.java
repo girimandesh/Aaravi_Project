c0d8fd7c6a067a02a57f7a36efe2e00c522b4112
Owner <Owner@Mandesh.fios-router.home>
2020-09-16 09:56:19 -0400

    new change

--- Project_Java/src/com/projectJava/newTest.java
+++ Project_Java/src/com/projectJava/newTest.java
@@ -4,6 +4,7 @@ public class newTest {
 	public static void main(String[] args) {
 		System.out.println("This is for Testing");
 		System.out.println("My Name is Mandesh Giri");
+		System.out.println(The name is Mandesh);
 	}
 
 }
